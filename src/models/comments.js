c414cb2b3bb032ae0a900ff1176f2e4736830dff
Lakesh kumar <lakeshkumar19@gmail.com>
2023-02-05 16:26:45 +0530

    added comments api

--- src/models/comments.js
+++ src/models/comments.js
@@ -6,9 +6,22 @@ const commentSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    username: {
-      type: String,
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
+    onModel: {
+      type: String,
+      required: true,
+      enum: ["Tweet", "Comment"],
+    },
+    commentable: {
+      type: mongoose.Schema.ObjectId,
+      required: true,
+      refPath: "onModel",
+    },
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
   },
   { timestamps: true }
 );

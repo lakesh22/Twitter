1868de25d85add151a2bd5a806e6790d760b070d
Lakesh kumar <lakeshkumar19@gmail.com>
2023-02-05 13:53:02 +0530

    toggle likes working

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import express from "express";
 import bodyParser from "body-parser";
 
 import { connect } from "./config/database.js";
+
+import LikeService from "./services/like-service.js";
+import { UserRepository, TweetRepository } from "./repository/index.js";
+
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,4 +17,13 @@ app.listen(3000, async () => {
   console.log("server started");
   await connect();
   console.log("mongo Db connected");
+
+  const userRepo = new UserRepository();
+  const tweetRepo = new TweetRepository();
+  const tweets = await tweetRepo.getAll(0, 10);
+
+  const users = await userRepo.getAll();
+  console.log(users);
+  const likeService = new LikeService();
+  await likeService.toggleLike(tweets[0].id, "Tweet", users[0].id);
 });
--- src/repository/tweet-repository.js
+++ src/repository/tweet-repository.js
@@ -1,6 +1,10 @@
 import Tweet from "../models/tweet.js";
+import CrudRepository from "./crud-repository.js";
 
-class TweetRepository {
+class TweetRepository extends CrudRepository {
+  constructor() {
+    super(Tweet);
+  }
   async create(data) {
     try {
       const tweet = await Tweet.create(data);
@@ -10,27 +14,9 @@ class TweetRepository {
     }
   }
 
-  async get(id) {
-    try {
-      const tweet = await Tweet.findById(id);
-      return tweet;
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
   async update(tweetid, data) {
     try {
       const tweet = await Tweet.findByIdAndUpdate(tweetid, data, { new: true });
-      return tweet;
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  async destroy(id) {
-    try {
-      const tweet = await Tweet.findByIdAndRemove(id);
       return tweet;
     } catch (error) {
       console.log(error);
@@ -49,6 +35,15 @@ class TweetRepository {
   async getAll(offset, limit) {
     try {
       const tweet = await Tweet.find().skip(offset).limit(limit);
+      return tweet;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async find(id) {
+    try {
+      const tweet = await Tweet.findById(id).populate({ path: "likes" });
       return tweet;
     } catch (error) {
       console.log(error);
